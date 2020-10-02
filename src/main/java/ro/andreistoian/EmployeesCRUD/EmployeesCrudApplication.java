27f11931ba82b83cea7bb34ac2bc829f231e6561
Andrei Stoian <stoianandrei980@gmail.com>
2020-10-02 15:52:20 +0300

    project set-up

---
+++ src/main/java/ro/andreistoian/EmployeesCRUD/EmployeesCrudApplication.java
@@ -0,0 +1,13 @@
+package ro.andreistoian.EmployeesCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/andreistoian/EmployeesCRUD/EmployeesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.andreistoian.EmployeesCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
